868a275c26f53ed11000658e6afe5b9b4d249a38
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-03-17 02:33:40 +0100

    Initial configuration of packages and shell scripts (#2)
    * Initial configuration
    
    * Move legacy shell files from https://github.com/andreoliwa/clitoolkit

--- roles/git/aliases.zsh
+++ roles/git/aliases.zsh
@@ -1,22 +1,18 @@
 alias g="git"
 alias gf="git flow"
 
-# Override prezto
-alias gs="git status"
-alias gl="git pull"
-
 # Some shortcuts I used with Oh My Zsh
 alias gaa='git add --all'
 alias gcd='git checkout develop'
 alias gci='git-checkout-issue'
 alias gcm='git checkout master'
-alias gdd='git diff develop'
 alias gdm='git diff master'
-alias gld='git log ...develop'
+alias gl="git pull"
 alias glm='git log ...master'
+alias gs="git status"
 alias gst="git status"
 alias gsta='git add -A; git stash'
 alias gstl='git stash list'
 alias gstp='git stash pop'
-alias gwip='git add -A; git ls-files --deleted -z | xargs git rm; git commit -m "--wip--"'
 alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
+alias gwip='git add -A; git ls-files --deleted -z | xargs git rm; git commit -m "--wip--"'
