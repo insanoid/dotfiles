037d7a652f6e43cc96b4062ca9e5550cc1bceba6
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-09-06 14:21:24 +0200

    Add my custom zprezto prompt

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -17,7 +17,8 @@ alias v="vim"
 # resize images
 alias resize="mogrify -resize"
 
-alias grepalias='alias | grep '
+alias ef='hello work'
+alias h='hello home'
 
 alias ef='hello work'
 alias h='hello home'
