ee1aa4434e4c16e7a1440b6631f28f1924f5783c
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-09-06 14:24:15 +0200

    My own role for Python with pip and virtualenv (still a work in progress)

--- roles/python_virtualenv/aliases.zsh
+++ roles/python_virtualenv/aliases.zsh
@@ -8,9 +8,15 @@ alias pyserv="python3 -m http.server"
 alias pt="py.test"
 alias nt="nosetests"
 alias mn="python manage.py"
-alias pipgrep="pip freeze | grep -i "
 alias getpip="wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm get-pip.py"
 
 
 alias pf='pip freeze'
 alias wk=workon
+
+# My aliases
+alias grpip="pip3 freeze | rg -i "
+alias grpip2="pip2 freeze | rg -i "
+alias pf='pip3 freeze'
+alias pf2='pip2 freeze'
+alias pf3='pip3 freeze'
