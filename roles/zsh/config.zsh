593c10407874f8bd13bb5f8a262ea5cddec77af0
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-10-08 22:22:16 +0200

    Fix online help only for MacOS

--- roles/zsh/config.zsh
+++ roles/zsh/config.zsh
@@ -5,8 +5,10 @@ unsetopt correct_all
 
 # ==> Caveats
 # Add the following to your zshrc to access the online help:
-unalias run-help
-autoload run-help
-HELPDIR=/usr/local/share/zsh/help
+if [[ $OSTYPE == darwin* ]]; then
+    unalias run-help
+    autoload run-help
+    HELPDIR=/usr/local/share/zsh/help
+fi
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
